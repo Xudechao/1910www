07d5f7d79cf9db42f12f6f56164856991f9deb23
xudechao <3012922730@qq.com>
2020-06-19 20:26:07 +0800

    题

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
@@ -10,5 +11,13 @@ class TestController extends Controller
     {
         echo __METHOD__;echo '<br>';
         echo date('Y-m-d H:i:s');
+    }
+
+    //redis测试
+    public function redis1()
+    {
+        $key = 'name1';
+        $val1 = Redis::get($key);
+        echo '$val1'. $val1;
     }
 }
