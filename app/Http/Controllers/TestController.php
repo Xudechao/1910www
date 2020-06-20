64bf4b5483f477a6710eb214d2aa6fe95e02d508
xudechao <3012922730@qq.com>
2020-06-20 09:33:51 +0800

    哎

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,10 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
-<<<<<<< HEAD
-=======
-
->>>>>>> 76817d498484cc128524ebe756a4791ab90756a5
 
 class TestController extends Controller
 {
@@ -17,7 +13,7 @@ class TestController extends Controller
         echo date('Y-m-d H:i:s');
     }
 
-<<<<<<< HEAD
+
     //redis测试
     public function redis1()
     {
@@ -25,13 +21,5 @@ class TestController extends Controller
         $val1 = Redis::get($key);
         echo '$val1'. $val1;
     }
-=======
-     //redis测试
-    public function redis1()
-    {
-         $key = 'name1';
-         $val1 = Redis::get($key);
-  	 echo '$val1'. $val1;
-     }
->>>>>>> 76817d498484cc128524ebe756a4791ab90756a5
+
 }
